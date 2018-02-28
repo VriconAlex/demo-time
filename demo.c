7769b6f757e12366b8d411503587a0471bf6f4cb
Alex Alexandria <vriconalex@gmail.com>
2018-02-28 09:13:23 +0100

    cleanup

--- demo.c
+++ demo.c
@@ -1,16 +1,6 @@
 #include <stdio.h>
 
-void itertext(int n, char *text){
-  int i = 0;
-  if(n > 0){
-    for(i= 0; i < n; i++){
-      printf("%d %s\n", i, text);
-    }
-  }
-  return;
-}
 int main(){
-   // printf() displays the string inside quotation
    printf("Hello, World!\n");
    return 0;
 }
