2f5d491b9fb719d5bff4be3f5af9c00857c37c3a
Alex Alexandria <vriconalex@gmail.com>
2018-03-01 10:04:23 +0100

    ny funk

--- demo.c
+++ demo.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 
+void itertext(int n, char *text){
+  int i = 0;
+  if(n > 0){
+    for(i= 0; i < n; i++){
+      printf("%d %s\n", i, text);
+    }
+  }
+  return;
+}
+
 int main(){
-   printf("Hello, World!\n");
-   return 0;
+
+  printf("Hello, World!\n");
+  
+  return 0;
 }
